492be69bcfc044523ee7e85d199beb300ea9f59e
Ümit Tengiz <umitengiz@gmail.com>
2021-04-11 03:23:41 +0300

    Ant Design installed

--- vue/src/main.js
+++ vue/src/main.js
@@ -7,9 +7,13 @@ import store from './store'
 import VueApollo from "vue-apollo";
 import apolloClient from "./vue-apollo";
 
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
 Vue.config.productionTip = false
 
 Vue.use(VueApollo);
+Vue.use(Antd);
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient
