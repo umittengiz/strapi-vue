2a979c8852abeeb5411d984c8caa598acbc12697
Ümit Tengiz <umitengiz@gmail.com>
2021-05-31 06:29:47 +0300

    Cart components created

---
+++ waiter/src/components/cart/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Cart</h1>
+    <p>Products</p>
+    <p>Products</p>
+    <p>Products</p>
+    <p>Products</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cart",
+  components: {},
+  // data() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ waiter/src/components/products/Products.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Products</h1>
+    <p>Items | <span>Price</span></p>
+    <p>Items | <span>Price</span></p>
+    <p>Items | <span>Price</span></p>
+    <p>Items | <span>Price</span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Products",
+  components: {},
+  // data() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
