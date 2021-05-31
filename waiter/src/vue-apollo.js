a34419a5b42ef2f864dfac3b1d3b1a7d622efb17
Ümit Tengiz <umitengiz@gmail.com>
2021-05-31 06:38:38 +0300

    Vue Apollo settings file created to integrate GraphQL

---
+++ waiter/src/vue-apollo.js
@@ -0,0 +1,21 @@
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  // I commented out process.env... part because of error
+  uri: /* process.env.VUE_APP_GRAPHQL_URL || */ "http://localhost:1337/graphql",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+export default apolloClient;
