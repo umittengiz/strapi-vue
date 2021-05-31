cfd543372ef2a4fef185319d4a3f6648a5fba58b
Ümit Tengiz <umitengiz@gmail.com>
2021-05-31 06:29:29 +0300

    Table component created

---
+++ waiter/src/components/tables/Tables.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Tables</h1>
+    <p>Tables 01 | 1</p>
+    <p>Tables 02 | 0</p>
+    <p>Tables 03 | 0</p>
+    <p>Tables 04 | 0</p>
+    <p>Tables 05 | 0</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tables",
+  components: {},
+  // data() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
