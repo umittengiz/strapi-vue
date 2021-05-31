164f4422d44a8e4f0f7027c7a61eaddf479a815e
Ümit Tengiz <umitengiz@gmail.com>
2021-05-31 06:42:22 +0300

    Apollo is ready in main.js, added and configured

--- waiter/src/main.js
+++ waiter/src/main.js
@@ -4,10 +4,20 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import VueApollo from "vue-apollo";
+import apolloClient from "./vue-apollo";
+
 Vue.config.productionTip = false;
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: (h) => h(App),
 }).$mount("#app");
